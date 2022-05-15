6e787b7d38574952d1f1fdce6f219b21cef54b3b
Kyle <kylejmbaylis@gmail.com>
2022-05-15 14:46:10 -0400

    added all functions, test for connection to database and console.table working

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// For console.table/db
+// For mysql2
 const db = require('./db/connection');
 // For Inquirer
 const inquirer = require('inquirer');
-// For mysql2
-const mysql = require('mysql2');
+// For console.table
+const cTable = require('console.table');
 
+// Tests connection to database
+db.query(`SELECT * FROM employee`, (err, result) => {
+    if(err) {
+        console.log(err);
+    }
+    console.table(result);
+});
