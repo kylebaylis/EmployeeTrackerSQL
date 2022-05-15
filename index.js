93183bd45605d0fb225e8416f4476e490be2c49c
Kyle <kylejmbaylis@gmail.com>
2022-05-15 15:47:01 -0400

    added options in application for view all

--- index.js
+++ index.js
@@ -6,9 +6,50 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 // Tests connection to database
+/*
 db.query(`SELECT * FROM employee`, (err, result) => {
     if(err) {
         console.log(err);
     }
     console.table(result);
-});
+}); */
+
+const employeeTracker = () => {
+    return inquirer.prompt([
+    {
+        name: 'start',
+        message: 'What would you like to do?',
+        type: 'list',
+        choices: ['View all departments', 'View all roles', 'View all employees',
+                 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+    }
+    ])
+    .then((answer) => {
+        if(answer.start == 'View all departments') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if(err) {
+                    console.log(err);
+                }
+                console.table(result);
+            });
+        }
+        if(answer.start == 'View all roles')  {
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if(err) {
+                    console.log(err);
+                }
+                console.table(result);
+            });
+        }
+        if(answer.start == 'View all employees') {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if(err) {
+                    console.log(err);
+                }
+                console.table(result);
+            });
+        }
+    });
+};
+
+employeeTracker();
